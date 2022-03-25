10046726d95db83e4babd32112a474ba12499022
Justin Yao Du <justinyaodu@gmail.com>
2022-03-25 10:50:13 -0700

    WIP: more validation

--- backend/src/validators/validators.ts
+++ backend/src/validators/validators.ts
@@ -1,49 +1,23 @@
+import e from "express";
+
 // https://stackoverflow.com/a/50375286
 type UnionToIntersection<U> = (U extends any ? (u: U) => void : never) extends ((u: infer I) => void) ? I : never;
 
-type AddOptionalIfMaybeUndefined<T> = UnionToIntersection<
+type UndefinedToOptional<T> = UnionToIntersection<
   {
-    [K in keyof T]: undefined extends T[K]
-      ? {[key in K]?: T[K]}
-      : {[key in K]: T[K]}
-  }[keyof T]
->;
-
-type ExcludeUndefined<T> = T extends undefined ? never : T;
-
-// Cannot fully remove undefined from optional fields, unless
-// --strictOptionalProperties is enabled
-type StripUndefined<T> = UnionToIntersection<
-  {
-    [K in keyof T]: T[K] extends undefined
-      ? {}
+    [K in keyof T]: (
+      T[K] extends undefined
+        ? {}
         : undefined extends T[K]
           ? {[key in K]?: T[K]}
           : {[key in K]: T[K]}
+    )
   }[keyof T]
 >;
 
-interface Aoeu {
-  a: boolean;
-  b: boolean | undefined;
-  c: undefined;
-}
+type TupleOf<T> = readonly [] | readonly [T, ...T[]];
 
-type StripAoeu = StripUndefined<Aoeu>;
-
-type Baz = Exclude<string | undefined, undefined>;
-
-type ExcludeUndefinedFromValues<T> = {[K in keyof T]: Exclude<T[K], undefined>};
-
-/*
-type DeleteUndefinedFields<T> = UnionToIntersection<
-  {
-    [K in keyof T]
-  }[keyof T]
->;
-*/
-
-type Primitive = bigint | boolean | number | null | string | symbol;
+type Primitive = bigint | boolean | null | number | string | symbol;
 
 function isPrimitive(input: unknown): input is Primitive {
   switch (typeof input) {
@@ -60,51 +34,61 @@ function isPrimitive(input: unknown): input is Primitive {
   }
 }
 
-type TransformFunction<I, O, E = I> = (input: I) => O;
+type Transformer<I, O, E = I> = (input: I) => O;
 
-interface ObjectTransformSpec {
-  // eslint-disable-next-line no-use-before-define
-  readonly [key: string]: Primitive | TransformFunction<unknown, unknown> | TupleTransformSpec | ObjectTransformSpec | undefined;
+interface ObjectSpec {
+  readonly [key: string]: Primitive | Transformer<unknown, unknown> | TupleSpec | ObjectSpec | undefined;
 }
 
-type TransformSpec = Exclude<ObjectTransformSpec[string], undefined>;
-type TupleTransformSpec = readonly [TransformSpec, ...TransformSpec[]] | readonly [];
+type TransformSpec = Exclude<ObjectSpec[string], undefined>;
+type TupleSpec = readonly [] | readonly [TransformSpec, ...TransformSpec[]];
 
-type TransformInput<S> = (
-  S extends TransformFunction<infer I, infer O, infer E>
+// all inputs that transform successfully should match this type - the more specific the better
+type NarrowestInput<S> = (
+  S extends Primitive
+  ? S
+  : S extends Transformer<infer I, infer O, infer E>
     ? I & E
-    : S extends Primitive
-      ? S
-      : S extends TupleTransformSpec
-        ? {readonly [K in keyof S]: K extends keyof [] ? S[K] : TransformInput<S[K]>}
-        : S extends ObjectTransformSpec
-          ? StripUndefined<{readonly [K in keyof S]: TransformInput<S[K]>}>
+      : S extends TupleSpec
+        ? {readonly [K in keyof S]: K extends keyof [] ? S[K] : NarrowestInput<S[K]>}
+        : S extends ObjectSpec
+          ? UndefinedToOptional<{readonly [K in keyof S]: NarrowestInput<S[K]>}>
           : never
 );
 
-type TransformOutput<S> = (
+// all inputs that can be validated by this transformer
+type WidestInput<S> = (
+  S extends Primitive
+  ? unknown
+  : S extends Transformer<infer I, infer O, infer E>
+    ? I
+      : S extends TupleSpec
+        ? unknown
+        : S extends ObjectSpec
+          ? unknown
+          : never
+);
+
+type Output<S> = (
   S extends Primitive
     ? S
-    : S extends TransformFunction<infer I, infer O, infer E>
+    : S extends Transformer<infer I, infer O, infer E>
       ? O
-      : S extends TupleTransformSpec
-        ? {-readonly [K in keyof S]: K extends keyof [] ? S[K] : TransformOutput<S[K]>}
-        : S extends ObjectTransformSpec
-          ? StripUndefined<{-readonly [K in keyof S]: TransformOutput<S[K]>}>
-          // ? {-readonly [K in keyof S]: undefined extends TransformOutput<S[K]> ? never : TransformOutput<S[K]>} & {-readonly [K in keyof S]+?: undefined extends TransformOutput<S[K]> ? TransformOutput<S[K]> : never}
+      : S extends TupleSpec
+        ? {-readonly [K in keyof S]: K extends keyof [] ? S[K] : Output<S[K]>}
+        : S extends ObjectSpec
+          ? UndefinedToOptional<{-readonly [K in keyof S]: Output<S[K]>}>
           : never
 );
 
-type foo = undefined extends (undefined | null) ? true : false;
-
-function transformPrimitive<S extends Primitive>(input: unknown, spec: S): TransformOutput<S> {
+function transformPrimitive<S extends Primitive>(input: WidestInput<S>, spec: S): Output<S> {
   if (input === spec) {
-    return input as TransformOutput<S>;
+    return spec as Output<S>;
   }
   throw new Error("Values do not match");
 }
 
-function transformTuple<S extends TupleTransformSpec>(input: unknown, spec: S): TransformOutput<S> {
+function transformTuple<S extends TupleSpec>(input: WidestInput<S>, spec: S): Output<S> {
   if (!Array.isArray(input)) {
     throw new Error("Not an array");
   }
@@ -113,11 +97,12 @@ function transformTuple<S extends TupleTransformSpec>(input: unknown, spec: S): 
   }
 
   // @ts-ignore
-  const transformed = [] as TransformOutput<S>;
+  const transformed = [] as Output<S>;
   for (let i = 0; i < spec.length; i++) {
     try {
+      const transformedValue = transform(input[i], spec[i]);
       // @ts-ignore
-      transformed.append(transform(input[i], spec[i]));
+      transformed.append(transformedValue);
     } catch (e) {
       throw e;
     }
@@ -125,70 +110,52 @@ function transformTuple<S extends TupleTransformSpec>(input: unknown, spec: S): 
   return transformed;
 }
 
-function transformObject<S extends ObjectTransformSpec>(input: unknown, spec: S): TransformOutput<S> {
+function transformObject<S extends ObjectSpec>(input: WidestInput<S>, spec: S): Output<S> {
   if (typeof input !== "object" || input === null) {
     throw new Error("Not an object");
   }
 
-  // @ts-ignore
-  const transformed = {} as TransformOutput<S>;
   for (const key of Object.getOwnPropertyNames(input)) {
-    if (key in spec && spec[key] !== undefined) {
-      // Field exists in input and spec.
-      // @ts-ignore
-      const value = transform(input[key], spec[key]);
-      if (value !== undefined) {
-        // @ts-ignore
-        transformed[key] = value;
-      }
-    } else {
-      // Field exists in input only.
+    if (!(key in spec)) {
       throw new Error("Extra key");
     }
   }
+
+  const transformed = {} as Output<S>;
   for (const key of Object.getOwnPropertyNames(spec)) {
-    if (!(key in input)) {
-      // Field exists in spec only.
-      // @ts-ignore
-      const value = transform(undefined, spec[key]);
-      if (value !== undefined) {
+    const valueSpec = spec[key];
+    if (valueSpec === undefined) {
+      continue;
+    }
+    const inputValue: unknown = (input as any)[key];
+    try {
+      const transformedValue = transform(inputValue, valueSpec);
+      if (transformedValue !== undefined) {
         // @ts-ignore
-        transformed[key] = value;
+        transformed[key] = transformedValue;
       }
+    } catch (e) {
+      throw e;
     }
   }
+
   return transformed;
 }
 
-function transform<S extends TransformSpec>(input: unknown, spec: S): TransformOutput<S> {
+function transform<S extends TransformSpec>(input: WidestInput<S>, spec: S): Output<S> {
   if (isPrimitive(spec)) {
-    return transformPrimitive(input, spec);
+    return transformPrimitive(input, spec) as Output<S>;
   }
   if (typeof spec === "function") {
-    return spec(input) as TransformOutput<S>;
+    return spec(input) as Output<S>;
   }
   if (Array.isArray(spec)) {
-    return transformTuple(input, spec as TupleTransformSpec) as TransformOutput<S>;
+    return transformTuple(input, spec as TupleSpec) as Output<S>;
   }
-  return transformObject(input, spec as ObjectTransformSpec) as TransformOutput<S>;
+  return transformObject(input, spec as ObjectSpec) as Output<S>;
 }
 
-
-/*
-function _boolean<I>(input: I) {
-  if (typeof input === "boolean") {
-    return input;
-  }
-  throw new Error();
-}
-
-const x = boolean("hi");
-const y = boolean(false);
-
-const boolean: TransformFunction<unknown, boolean, boolean> = _boolean;
-*/
-
-const boolean: TransformFunction<unknown, boolean, boolean> = (input) => {
+const boolean: Transformer<unknown, boolean, boolean> = (input) => {
   if (typeof input === "boolean") {
     return input;
   } else {
@@ -196,21 +163,21 @@ const boolean: TransformFunction<unknown, boolean, boolean> = (input) => {
   }
 }
 
-const number: TransformFunction<unknown, number, number> = (input) => {
+const number: Transformer<unknown, number, number> = (input) => {
   if (typeof input === "number") {
     return input;
   }
   throw new Error();
 }
 
-const string: TransformFunction<unknown, string, string> = (input) => {
+const string: Transformer<unknown, string, string> = (input) => {
   if (typeof input === "string") {
     return input;
   }
   throw new Error();
 }
 
-function optional<S extends TransformSpec>(spec: S): TransformFunction<unknown, TransformOutput<S> | undefined, TransformInput<S> | undefined> {
+function optional<S extends TransformSpec>(spec: S): Transformer<WidestInput<S> | undefined, Output<S> | undefined, NarrowestInput<S> | undefined> {
   return (input) => {
     if (input === undefined) {
       return undefined;
@@ -218,6 +185,145 @@ function optional<S extends TransformSpec>(spec: S): TransformFunction<unknown, 
     return transform(input, spec);
   };
 }
+
+/*
+function union<S extends TransformSpec, T extends TransformSpec>(first: S, second: T): TransformFunction<unknown, TransformOutput<S> | TransformOutput<T>, TransformInput<S> | TransformInput<T>> {
+  return (input) => {
+    try {
+      return transform(input, first);
+    } catch (e) {
+      return transform(input, second);
+    }
+  }
+}
+*/
+
+/*
+type UnionSpec = TransformSpec | Transformer<never, unknown>;
+
+// @ts-ignore
+function union<S extends [UnionSpec, ...UnionSpec[]]>(...specs: S): Transformer<UnionToIntersection<WidestInput<S>[number]>, Output<S>[number], NarrowestInput<S>[number]> {
+  return (input) => {
+    for (let i = 0; i < specs.length; i++) {
+      try {
+        const spec = specs[i];
+        if (typeof spec === "function") {
+          return spec(input as never) as any;
+        } else {
+          return transform(input, spec);
+        }
+      } catch (e) {
+        throw e;
+      }
+    }
+  }
+}
+*/
+
+function union<S extends readonly [TransformSpec, ...TransformSpec[]]>(...specs: S): Transformer<
+    unknown, // UnionToIntersection<{ [K in keyof S]: K extends keyof [] ? never : WidestInput<S[K]> }[keyof S]>,
+    Output<S>[number],
+    // @ts-ignore
+    NarrowestInput<S>[number], // { [K in keyof S]: K extends keyof [] ? never : NarrowestInput<S[K]> }[keyof S]
+> {
+  return (input) => {
+    for (let i = 0; i < specs.length; i++) {
+      try {
+        return transform(input, specs[i]);
+      } catch (e) {
+        throw e;
+      }
+    }
+  }
+}
+
+/*
+function union2<I, O, E, S extends readonly [Transformer<I, O, E>, ...Transformer<I, O, E>[]]>(...transformers: S): Transformer<
+  I, O, E
+> {
+  return (input) => {
+    for (let i = 0; i < transformers.length; i++) {
+      try {
+        return transformers[i](input);
+      } catch (e) {
+        throw e;
+      }
+    }
+    throw new Error();
+  }
+}
+
+function union3<I, S extends readonly [(input: any) => any, ...((input: any) => any)[]]>(...transformers: S): Transformer<
+  Parameters<S[number]>[0],
+  ReturnType<S[number]>,
+  NarrowestInput<S[number]>> {
+    return (input) => {
+      for (let i = 0; i < transformers.length; i++) {
+        try {
+          return transformers[i](input);
+        } catch (e) {
+          throw e;
+        }
+      }
+    throw new Error();
+    }
+}
+*/
+
+function transformerUnion<I, O, E = I>(...transformers: Transformer<I, O, E>[]): Transformer<I, O, E> {
+  return (input) => {
+    for (let i = 0; i < transformers.length; i++) {
+      try {
+        return transformers[i](input);
+      } catch (e) {
+        throw e;
+      }
+    }
+    throw new Error();
+  }
+}
+
+// function union3<S extends readonly [Transformer, ...Transformer[]]>(...transformers: S): Transformer<
+
+/*
+function unionTransform<S extends [Transformer<never, unknown>, ...Transformer<never, unknown>[]]>(...transformers: S): Transformer<
+    UnionToIntersection<{ [K in keyof S]: K extends keyof [] ? never : WidestInput<S[K]> }[keyof S]>,
+    { [K in keyof S]: K extends keyof [] ? never : Output<S[K]> }[keyof S],
+    { [K in keyof S]: K extends keyof [] ? never : NarrowestInput<S[K]> }[keyof S]> {
+  return (input) => {
+    for (let i = 0; i < transformers.length; i++) {
+      try {
+        return transformers[i](input as never) as any;
+      } catch (e) {
+        throw e;
+      }
+    }
+  }
+}
+*/
+
+const fooSpec = {
+  baz: union(string, number, optional(boolean)),
+  test: union(number, string),
+  // stuff: union2<unknown, number | string, number | string, [typeof number, typeof string]>(number, string),
+  rad: transformerUnion<unknown, number | string, number | string>(number, string), // union3(number, string, (x: string) => x + "hi"),
+  brob: transformerUnion<string, number | Date>((x: string) => parseInt(x), (x: string) => new Date(x)),
+};
+const test: ObjectSpec = fooSpec;
+
+// const fooSpec = (bar: string) => parseInt(bar);
+
+type FooWide = WidestInput<typeof fooSpec>;
+type FooNarrow = NarrowestInput<typeof fooSpec>;
+type FooOutput = Output<typeof fooSpec>;
+
+/*
+const bar = union3(number, (x: string) => x + "hi"); // unionTransform((x: string) => parseInt(x));
+
+type BarWide = WidestInput<typeof bar>;
+type BarNarrow = NarrowestInput<typeof bar>;
+type BarOutput = Output<typeof bar>;
+*/
 
 const numberValidationSpec = {
   min: optional(number),
@@ -227,36 +333,22 @@ const numberValidationSpec = {
   foo: undefined,
 };
 
-type NumberValidationInput = TransformInput<typeof numberValidationSpec>;
-type NumberValidationOutput = TransformOutput<typeof numberValidationSpec>;
+type NumberValidationInput = NarrowestInput<typeof numberValidationSpec>;
+type NumberValidationOutput = Output<typeof numberValidationSpec>;
 
-type MinValidation = TransformOutput<typeof numberValidationSpec["min"]>;
+type MinValidation = Output<typeof numberValidationSpec["min"]>;
 
-type NumberValidation = TransformOutput<typeof numberValidationSpec>;
+type NumberValidation = Output<typeof numberValidationSpec>;
 type bar = NumberValidation["min"];
 
-type baz = TransformInput<typeof numberValidationSpec>;
+type baz = NarrowestInput<typeof numberValidationSpec>;
 
 type Test = {
   a: boolean;
   b: number | undefined;
 }
 
-// type UndefinedToOptional<T> = {[K in keyof T]: undefined extends T[K] ? {[key: K]: T[K]} : {[key: K]: T[K]}};
-// type OptionalEntries<T> = {[K in keyof T]: undefined extends T[K] ? /*[true, K, T[K]]*/ {[key in K]: T[K]} : /*[false, K, T[K]]*/ {[key in K]: T[K]}}[keyof T];
-
-// type Aoeu = AllowMissingIfUndefined<Test>;
-
-// type Bar = Omit<Test, keyof Test>;
-
-// type Stuff = { [K in keyof Test]: undefined extends Test[K] ? any : Test[K] } & { [K in keyof Test]+?: undefined extends Test[K] ? Test[K] : any };
-// type Stuff = { [K in keyof Test]: undefined extends Test[K] ? any : Test[K] } & { [K in keyof Test]+?: undefined extends Test[K] ? Test[K] : any };
-
-// const x: Stuff = { a: true };
-// type AType = Stuff["a"];
-// type BType = Stuff["b"];
-
-function validatedNumber(options: NumberValidationInput): TransformFunction<unknown, number> {
+function validatedNumber(options: NumberValidationInput): Transformer<unknown, number> {
   return (input) => {
     const num = number(input);
     if (options.min !== undefined && !(options.min <= num)) {
@@ -267,7 +359,7 @@ function validatedNumber(options: NumberValidationInput): TransformFunction<unkn
   };
 }
 
-const foo = validatedNumber({ max: 2 })(7);
+const foo = validatedNumber({ real: false })(7);
 
 export {
   number,
