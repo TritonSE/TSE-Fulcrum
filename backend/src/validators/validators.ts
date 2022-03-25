285d75c08b735e0ec45009236408e2877842718f
Justin Yao Du <justinyaodu@gmail.com>
2022-03-25 13:48:10 -0700

    WIP: even more validation

--- backend/src/validators/validators.ts
+++ backend/src/validators/validators.ts
@@ -15,8 +15,6 @@ type UndefinedToOptional<T> = UnionToIntersection<
   }[keyof T]
 >;
 
-type TupleOf<T> = readonly [] | readonly [T, ...T[]];
-
 type Primitive = bigint | boolean | null | number | string | symbol;
 
 function isPrimitive(input: unknown): input is Primitive {
@@ -43,7 +41,6 @@ interface ObjectSpec {
 type TransformSpec = Exclude<ObjectSpec[string], undefined>;
 type TupleSpec = readonly [] | readonly [TransformSpec, ...TransformSpec[]];
 
-// all inputs that transform successfully should match this type - the more specific the better
 type NarrowestInput<S> = (
   S extends Primitive
   ? S
@@ -56,7 +53,6 @@ type NarrowestInput<S> = (
           : never
 );
 
-// all inputs that can be validated by this transformer
 type WidestInput<S> = (
   S extends Primitive
   ? unknown
@@ -155,6 +151,10 @@ function transform<S extends TransformSpec>(input: WidestInput<S>, spec: S): Out
   return transformObject(input, spec as ObjectSpec) as Output<S>;
 }
 
+function compile<S extends TransformSpec>(spec: S): Transformer<WidestInput<S>, Output<S>, NarrowestInput<S>> {
+  return (input) => transform(input, spec);
+}
+
 const boolean: Transformer<unknown, boolean, boolean> = (input) => {
   if (typeof input === "boolean") {
     return input;
@@ -177,6 +177,16 @@ const string: Transformer<unknown, string, string> = (input) => {
   throw new Error();
 }
 
+function replaceWith<T>(value: T): Transformer<unknown, T> {
+  return (input) => value;
+}
+
+const remove = replaceWith(undefined);
+
+const placeholder = ((input) => {
+  throw new Error();
+}) as TransformSpec;
+
 function optional<S extends TransformSpec>(spec: S): Transformer<WidestInput<S> | undefined, Output<S> | undefined, NarrowestInput<S> | undefined> {
   return (input) => {
     if (input === undefined) {
@@ -186,45 +196,11 @@ function optional<S extends TransformSpec>(spec: S): Transformer<WidestInput<S> 
   };
 }
 
-/*
-function union<S extends TransformSpec, T extends TransformSpec>(first: S, second: T): TransformFunction<unknown, TransformOutput<S> | TransformOutput<T>, TransformInput<S> | TransformInput<T>> {
-  return (input) => {
-    try {
-      return transform(input, first);
-    } catch (e) {
-      return transform(input, second);
-    }
-  }
-}
-*/
-
-/*
-type UnionSpec = TransformSpec | Transformer<never, unknown>;
-
-// @ts-ignore
-function union<S extends [UnionSpec, ...UnionSpec[]]>(...specs: S): Transformer<UnionToIntersection<WidestInput<S>[number]>, Output<S>[number], NarrowestInput<S>[number]> {
-  return (input) => {
-    for (let i = 0; i < specs.length; i++) {
-      try {
-        const spec = specs[i];
-        if (typeof spec === "function") {
-          return spec(input as never) as any;
-        } else {
-          return transform(input, spec);
-        }
-      } catch (e) {
-        throw e;
-      }
-    }
-  }
-}
-*/
-
 function union<S extends readonly [TransformSpec, ...TransformSpec[]]>(...specs: S): Transformer<
-    unknown, // UnionToIntersection<{ [K in keyof S]: K extends keyof [] ? never : WidestInput<S[K]> }[keyof S]>,
+    unknown,
     Output<S>[number],
     // @ts-ignore
-    NarrowestInput<S>[number], // { [K in keyof S]: K extends keyof [] ? never : NarrowestInput<S[K]> }[keyof S]
+    NarrowestInput<S>[number],
 > {
   return (input) => {
     for (let i = 0; i < specs.length; i++) {
@@ -237,40 +213,47 @@ function union<S extends readonly [TransformSpec, ...TransformSpec[]]>(...specs:
   }
 }
 
-/*
-function union2<I, O, E, S extends readonly [Transformer<I, O, E>, ...Transformer<I, O, E>[]]>(...transformers: S): Transformer<
-  I, O, E
-> {
-  return (input) => {
-    for (let i = 0; i < transformers.length; i++) {
-      try {
-        return transformers[i](input);
-      } catch (e) {
-        throw e;
-      }
-    }
-    throw new Error();
-  }
-}
+type RecursiveTransformerUnion<I, O, E, S> = (
+  S extends readonly []
+  ? [I, O, E]
+  : S extends readonly [infer T, ...infer R]
+    ? T extends Transformer<infer J, infer P, infer F>
+      ? RecursiveTransformerUnion<I & WidestInput<T>, O | Output<T>, E | NarrowestInput<T>, R>
+      : never
+    : never
+);
 
-function union3<I, S extends readonly [(input: any) => any, ...((input: any) => any)[]]>(...transformers: S): Transformer<
-  Parameters<S[number]>[0],
-  ReturnType<S[number]>,
-  NarrowestInput<S[number]>> {
-    return (input) => {
-      for (let i = 0; i < transformers.length; i++) {
-        try {
-          return transformers[i](input);
-        } catch (e) {
-          throw e;
-        }
-      }
-    throw new Error();
-    }
-}
+type TransformerUnion<S> = RecursiveTransformerUnion<unknown, never, never, S>;
+
+type RecursiveTransformerChain<I, O, E, S> = (
+  S extends readonly []
+    ? [I, O, E]
+    : S extends readonly [infer T, ...infer R]
+      ? T extends Transformer<O, infer P, infer F>
+        ? RecursiveTransformerChain<I, P, E, R>
+        : never
+      : never
+);
+
+type TransformerChain<S> = (
+  S extends [infer T, ...infer R]
+    ? T extends Transformer<infer I, infer O, infer E>
+      ? RecursiveTransformerChain<WidestInput<T>, Output<T>, NarrowestInput<T>, R>
+      : never
+    : never
+);
+
+/*
+type test = RecursiveTransformerUnion<unknown, never, never, []>;
+type test2 = RecursiveTransformerUnion<unknown, never, never, [typeof number, typeof string]>;
 */
 
+/*
+function transformerUnion<S extends Transformer<WidestInput<S>, Output<S>>, T extends Transformer<WidestInput<T>, Output<T>>>(first: S, second: T): Transformer<WidestInput<S> & WidestInput<T>, Output<S> | Output<T>, NarrowestInput<S> | NarrowestInput<T>>;
+function transformerUnion<I, O, E = I>(...transformers: Transformer<I, O, E>[]): Transformer<I, O, E>;
 function transformerUnion<I, O, E = I>(...transformers: Transformer<I, O, E>[]): Transformer<I, O, E> {
+*/
+function transformerUnion<S extends [Function, ...Function[]]>(...transformers: S): Transformer<TransformerUnion<S>[0], TransformerUnion<S>[1], TransformerUnion<S>[2]> {
   return (input) => {
     for (let i = 0; i < transformers.length; i++) {
       try {
@@ -283,55 +266,104 @@ function transformerUnion<I, O, E = I>(...transformers: Transformer<I, O, E>[]):
   }
 }
 
-// function union3<S extends readonly [Transformer, ...Transformer[]]>(...transformers: S): Transformer<
+const frob = transformerUnion((x: string) => x);
+const aoeu = transformerUnion((x: string) => x + "nice", (y: unknown) => 3);
 
 /*
-function unionTransform<S extends [Transformer<never, unknown>, ...Transformer<never, unknown>[]]>(...transformers: S): Transformer<
-    UnionToIntersection<{ [K in keyof S]: K extends keyof [] ? never : WidestInput<S[K]> }[keyof S]>,
-    { [K in keyof S]: K extends keyof [] ? never : Output<S[K]> }[keyof S],
-    { [K in keyof S]: K extends keyof [] ? never : NarrowestInput<S[K]> }[keyof S]> {
-  return (input) => {
-    for (let i = 0; i < transformers.length; i++) {
-      try {
-        return transformers[i](input as never) as any;
-      } catch (e) {
-        throw e;
-      }
-    }
-  }
+function chain<I, J, S extends Transformer<I, J>, T extends Transformer<J, Output<T>>>(first: S, second: T): Transformer<I, Output<T>, NarrowestInput<S>> {
+  return (input) => second(first(input));
 }
 */
+
+/*
+function chain<S extends TransformSpec, T extends Transformer<Output<S>, Output<T>>>(first: S, second: T): Transformer<WidestInput<S>, Output<T>, NarrowestInput<S>> {
+  return (input) => second(transform(input, first));
+}
+*/
+
+type TransformerChain2<S extends TransformSpec, T extends Function[]> = TransformerChain<[Transformer<WidestInput<S>, Output<S>, NarrowestInput<S>>, ...T]>;
+
+// type epic = TransformerChain2<{hi: 7}, [(x: number) => string]>;
+
+function chain<S extends TransformSpec, T extends Parameters<typeof transformerChain>>(spec: S, ...transformers: T): Transformer<TransformerChain2<S, T>[0], TransformerChain2<S, T>[1], TransformerChain2<S, T>[2]> {
+  return (input) => {
+    let result = transform(input, spec);
+    for (const transformer of transformers) {
+      result = transformer(input);
+    }
+    return result as any;
+  }
+  // return (input) => transformerChain(...transformers)(transform(input, spec));
+}
+
+/*
+function transformerChain<S extends Transformer<WidestInput<S>, Output<S>>, T extends Transformer<Output<S>, Output<T>>>(first: S, second: T): Transformer<WidestInput<S>, Output<T>, NarrowestInput<S>> {
+  return (input) => second(first(input));
+}
+*/
+
+function transformerChain<S extends [Function, ...Function[]]>(...transformers: S): Transformer<TransformerChain<S>[0], TransformerChain<S>[1], TransformerChain<S>[2]> {
+  return (input) => {
+    for (const transformer of transformers) {
+      input = transformer(input);
+    }
+    return input;
+  }
+}
+
+const banana = transformerChain((x: string) => x + "hi", (y: number) => y + 3);
 
 const fooSpec = {
   baz: union(string, number, optional(boolean)),
   test: union(number, string),
-  // stuff: union2<unknown, number | string, number | string, [typeof number, typeof string]>(number, string),
-  rad: transformerUnion<unknown, number | string, number | string>(number, string), // union3(number, string, (x: string) => x + "hi"),
-  brob: transformerUnion<string, number | Date>((x: string) => parseInt(x), (x: string) => new Date(x)),
+  rad: transformerUnion/*<unknown, number | string | boolean, number | string | boolean>*/(number, string, boolean),
+  // brob: transformerUnion<string, number | Date>((x: string) => parseInt(x), (x: string) => new Date(x)),
 };
 const test: ObjectSpec = fooSpec;
-
-// const fooSpec = (bar: string) => parseInt(bar);
 
 type FooWide = WidestInput<typeof fooSpec>;
 type FooNarrow = NarrowestInput<typeof fooSpec>;
 type FooOutput = Output<typeof fooSpec>;
 
-/*
-const bar = union3(number, (x: string) => x + "hi"); // unionTransform((x: string) => parseInt(x));
+const linkedListSpec = {
+  value: chain(number, (x: number) => x + 1, (y: number) => y + "hi"),
+  baz: chain([number, number], transformerChain((([x, y]: [number, number]) => x + y), ((z: number) => z + 3))),
+  next: placeholder,
+}
 
-type BarWide = WidestInput<typeof bar>;
-type BarNarrow = NarrowestInput<typeof bar>;
-type BarOutput = Output<typeof bar>;
-*/
+linkedListSpec.next = optional(linkedListSpec);
+
+type LinkedListInput = NarrowestInput<typeof linkedListSpec> & {
+  readonly next?: LinkedListInput;
+};
+
+type LinkedListOutput = Output<typeof linkedListSpec> & {
+  next?: LinkedListOutput;
+}
+
+const transformLinkedList = compile(linkedListSpec) as Transformer<unknown, LinkedListOutput, LinkedListInput>;
+
+const thing: LinkedListInput = {
+  value: 5,
+  baz: [3, 9],
+  next: {
+    value: 7,
+    baz: [2, 4],
+  },
+};
 
 const numberValidationSpec = {
   min: optional(number),
-  max: optional(number),
-  integer: optional(boolean),
+  // max: optional(number),
+  // integer: optional(boolean),
   real: boolean,
   foo: undefined,
+  nice: remove,
+  awesome: replaceWith(7),
+  nested: placeholder,
 };
+
+numberValidationSpec.nested = optional(numberValidationSpec);
 
 type NumberValidationInput = NarrowestInput<typeof numberValidationSpec>;
 type NumberValidationOutput = Output<typeof numberValidationSpec>;
