7746d7e5211a624236be93c4326a62530148b38c
Justin Yao Du <justinyaodu@gmail.com>
2022-10-04 22:08:24 -0700

    Sort reviews by stage, name, and reviewer

--- frontend/src/views/ReviewsView.tsx
+++ frontend/src/views/ReviewsView.tsx
@@ -28,13 +28,47 @@ function AutoAssignButton({
   );
 }
 
+/**
+ * Create a comparator for sorting objects.
+ * @param keyFunc Map each object to an array of values to sort by.
+ */
+function makeComparator<T, K extends (number | string)[]>(
+  keyFunc: (v: T) => K
+): (v1: T, v2: T) => number {
+  return (value1, value2) => {
+    const key1 = keyFunc(value1);
+    const key2 = keyFunc(value2);
+    for (let i = 0; i < key1.length; i++) {
+      if (key1[i] < key2[i]) return -1;
+      if (key1[i] > key2[i]) return 1;
+    }
+    return 0;
+  };
+}
+
 export default function ReviewsView({ filter }: { filter: Record<string, string> }) {
   const [reviews, setReviews] = useState<PopulatedReview[]>([]);
   const { alerts, addAlert } = useAlerts();
 
   // TODO: audit all other useEffects to check dependency lists
   useEffect(() => {
-    api.getFilteredReviews(filter).then(setReviews).catch(addAlert);
+    api
+      .getFilteredReviews(filter)
+      .then((newReviews) =>
+        setReviews(
+          newReviews
+            .slice()
+            .sort(
+              makeComparator((r) => [
+                r.stage.name,
+                r.application.name,
+                r.reviewerEmail || "",
+                r._id,
+              ])
+            )
+        )
+      )
+      .catch(addAlert);
   }, [filter]);
 
   return (
