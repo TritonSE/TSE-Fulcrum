451293c99e9943380015863cd59b728df532fc93
Justin Yao Du <justinyaodu@gmail.com>
2022-10-15 11:59:21 -0700

    Sort reviews by pipeline and stage index

--- frontend/src/pages/ViewApplication.tsx
+++ frontend/src/pages/ViewApplication.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 
 import api, { PopulatedReview } from "../api";
 import { useAlerts } from "../hooks";
+import { makeComparator } from "../util";
 import ApplicationView from "../views/ApplicationView";
 
 import { ReviewView } from "./EditReview";
@@ -22,9 +23,14 @@ export default function ViewApplication() {
   return (
     <>
       <ApplicationView id={applicationId || ""} />
-      {reviews.map((r) => (
-        <ReviewView key={r._id} id={r._id} editable={false} showApplication={false} />
-      ))}
+      {reviews
+        .slice()
+        .sort(
+          makeComparator((r) => [r.stage.pipeline, r.stage.pipelineIndex, r.reviewerEmail || ""])
+        )
+        .map((r) => (
+          <ReviewView key={r._id} id={r._id} editable={false} showApplication={false} />
+        ))}
       {alerts}
     </>
   );
