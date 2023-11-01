422e087b259e2e37bb172230adc463cad653b594
Andrew <Cubified@users.noreply.github.com>
2023-10-31 22:44:22 -0700

    Fix infinite loop bug, add portable emoji

--- frontend/src/pages/Interview.tsx
+++ frontend/src/pages/Interview.tsx
@@ -126,7 +126,7 @@ function Timer({ role, since, start }: TimerProps) {
     // See consistent-return eslint rule
     if (since === 0) return () => {};
 
-    const timeout = setTimeout(() => setTimeLeft(Math.max(0, timeLeft - SECOND)), SECOND);
+    const timeout = setTimeout(() => setTimeLeft(since + INTERVIEW_DURATION - Date.now()), SECOND);
 
     return () => clearTimeout(timeout);
   }, [timeLeft]);
@@ -139,9 +139,24 @@ function Timer({ role, since, start }: TimerProps) {
   const view = visible ? (
     <span>{format()}&nbsp;(click to hide)</span>
   ) : (
-    <span role="img" aria-label="Timer">
-      ⌛
-    </span>
+    <svg
+      style={{ width: "1rem", height: "1rem" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 36 36"
+    >
+      <path
+        fill="#FFE8B6"
+        d="M21 18c0-2.001 3.246-3.369 5-6 2-3 2-10 2-10H8s0 7 2 10c1.754 2.631 5 3.999 5 6s-3.246 3.369-5 6c-2 3-2 10-2 10h20s0-7-2-10c-1.754-2.631-5-3.999-5-6z"
+      />
+      <path
+        fill="#FFAC33"
+        d="M20.999 24c-.999 0-2.057-1-2.057-2C19 20.287 19 19.154 19 18c0-3.22 3.034-4.561 4.9-7H12.1c1.865 2.439 4.9 3.78 4.9 7 0 1.155 0 2.289.058 4 0 1-1.058 2-2.058 2-2 0-3.595 1.784-4 3-1 3-1 7-1 7h16s0-4-1-7c-.405-1.216-2.001-3-4.001-3z"
+      />
+      <path
+        fill="#3B88C3"
+        d="M30 34c0 1.104-.896 2-2 2H8c-1.104 0-2-.896-2-2s.896-2 2-2h20c1.104 0 2 .896 2 2zm0-32c0 1.104-.896 2-2 2H8c-1.104 0-2-.896-2-2s.896-2 2-2h20c1.104 0 2 .896 2 2z"
+      />
+    </svg>
   );
 
   return (
@@ -260,9 +275,9 @@ export default function Interview() {
         socket.emit("getState");
       }
     };
-  const updateEditorLanguage = (lang: string) => {
+  const updateEditorLanguage = (lang: string, send = false) => {
     setLanguage(lang);
-    sendMessage("language", lang);
+    if (send) sendMessage("language", lang);
 
     if (!codeEditor.current) return;
     const mod = codeEditor.current.editor.getModel();
@@ -299,15 +314,9 @@ export default function Interview() {
 
       mod.setValue(val);
     },
-    language: (payload: Payload) => {
-      updateEditorLanguage(payload.value as string);
-    },
-    active: (payload: Payload) => {
-      setActive(payload.value as boolean);
-    },
-    timerStart: (payload: Payload) => {
-      setTimerStart(payload.value as number);
-    },
+    language: (payload: Payload) => updateEditorLanguage(payload.value as string),
+    active: (payload: Payload) => setActive(payload.value as boolean),
+    timerStart: (payload: Payload) => setTimerStart(payload.value as number),
   };
 
   useEffect(() => {
@@ -397,7 +406,7 @@ export default function Interview() {
             <Dropdown.Toggle>Set Language</Dropdown.Toggle>
             <Dropdown.Menu>
               {LANGS.map((lang) => (
-                <Dropdown.Item key={lang} onClick={() => updateEditorLanguage(lang)}>
+                <Dropdown.Item key={lang} onClick={() => updateEditorLanguage(lang, true)}>
                   {lang[0].toUpperCase() + lang.slice(1)}
                 </Dropdown.Item>
               ))}
