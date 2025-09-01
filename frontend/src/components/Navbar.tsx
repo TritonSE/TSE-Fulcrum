103c047837ae10c9775a7a13b61819fe6423e1a6
yxli001 <lxd520anna@gmail.com>
2025-08-31 22:56:21 -0700

    finalize navbar styling and hide links from non-admins

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@tritonse/tse-constellation";
 import { useContext, useEffect, useMemo, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 import api, { Pipeline, Stage } from "../api";
 import { GlobalContext } from "../context/GlobalContext";
 
+function NavLink({ to, children }: { to: string; children: React.ReactNode }) {
+  const location = useLocation();
+
+  return (
+    <Link
+      className={twMerge(
+        "tw:!text-cream-primary tw:!no-underline tw:px-2 tw:py-1 tw:hover:!bg-white/10 tw:rounded-md",
+        location.pathname === to ? "tw:!bg-white/10" : ""
+      )}
+      to={to}
+    >
+      {children}
+    </Link>
+  );
+}
+
 function Navbar() {
   const { user, setUser } = useContext(GlobalContext);
-  const navigate = useNavigate();
   const userInitials = useMemo(
     () =>
       user?.name
@@ -18,12 +33,7 @@ function Navbar() {
     [user?.name]
   );
 
-  const onLogOut = () => {
-    api.logOut().then(() => {
-      setUser(null);
-      navigate("/login");
-    });
-  };
+  const navigate = useNavigate();
 
   const [pipelines, setPipelines] = useState<Pipeline[]>([]);
   // map pipeline name to stages
@@ -55,8 +65,15 @@ function Navbar() {
     });
   }, []);
 
+  const onLogOut = () => {
+    api.logOut().then(() => {
+      setUser(null);
+      navigate("/login");
+    });
+  };
+
   return (
-    <nav className="tw:bg-teal-primary tw:h-full tw:min-w-64 tw:flex tw:flex-col tw:gap-5 tw:px-6 tw:py-5 tw:text-cream-primary">
+    <nav className="tw:bg-teal-primary tw:h-full tw:min-w-64 tw:flex tw:flex-col tw:gap-5 tw:px-6 tw:pt-5 tw:pb-10 tw:text-cream-primary">
       {/* Logo */}
       <Link className="tw:flex tw:gap-3 tw:items-center tw:justify-center tw:!no-underline" to="/">
         <img src="/logo.svg" alt="Logo" className="tw:h-12 tw:w-4" />
@@ -66,37 +83,32 @@ function Navbar() {
       </Link>
 
       {/* Navigation links by pipeline and stage */}
-      <div className="tw:flex-1 tw:flex tw:flex-col tw:!text-sm tw:font-light">
-        {pipelines.map((pipeline, i) => (
-          <>
-            <div key={pipeline.identifier} className="tw:py-2 tw:flex tw:flex-col tw:gap-1">
+      {user?.isAdmin && (
+        <div className="tw:flex-1 tw:flex tw:flex-col tw:!text-sm tw:font-light">
+          <NavLink to="/reviews">
+            <span className="tw:uppercase">All Applicants</span>
+          </NavLink>
+          {pipelines.map((pipeline) => (
+            <div key={pipeline.identifier} className="tw:flex tw:flex-col">
+              {/* Separator */}
+              <div className="tw:w-[90%] tw:h-[1px] tw:bg-cream-primary/30 tw:mx-auto tw:my-3" />
               <h2 className="tw:text-cream-primary tw:!text-sm tw:font-bold tw:uppercase tw:!m-0 tw:px-2 tw:py-1">
                 {pipeline.name}
               </h2>
               <div className="tw:flex tw:flex-col tw:gap-1 tw:ml-2">
                 {stagesByPipeline[pipeline.name]?.map((stage) => (
-                  <Link
-                    key={stage.id}
-                    className={twMerge(
-                      "tw:!text-cream-primary tw:!no-underline tw:px-2 tw:py-1 tw:hover:!bg-white/10 tw:rounded"
-                    )}
-                    to={`/stages/${stage.id}`}
-                  >
+                  <NavLink key={stage.id} to={`/stage/${stage.id}/applications`}>
                     {stage.name}
-                  </Link>
+                  </NavLink>
                 ))}
               </div>
             </div>
-            {/* Separator */}
-            {i !== pipelines.length - 1 && (
-              <div className="tw:w-[90%] tw:h-[1px] tw:bg-cream-primary/30 tw:mx-auto" />
-            )}
-          </>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
 
       {/* Profile */}
-      <div className="tw:flex tw:flex-col tw:items-center tw:gap-3">
+      <div className="tw:flex tw:flex-col tw:items-center tw:gap-3 tw:mt-auto">
         <div className="tw:rounded-4xl tw:border-2 tw:border-cream-primary tw:p-3 tw:aspect-square tw:flex tw:items-center tw:justify-center">
           <span className="tw:font-bold">{userInitials}</span>
         </div>
